75e97fad773004505da9e77be00f6799be251e4a
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-02 15:55:49 +0930

    code

--- main-1-1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-extern int sum_diagonal(int array[4][4]);
-int main(){
-    int matrix[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-    cout << sum_diagonal(matrix) << endl;
-    return 0;
-}
