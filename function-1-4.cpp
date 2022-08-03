d3fdb8a55dbc82e1f71ba8feda715dcc9e81317c
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-03 13:56:02 +0930

    code

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void print_scaled(int array[3][3],int scale){
     for (int r = 0; r < 3; r++){
         for (int c = 0; c < 3; c++){
-            cout << array[r][c]* scale << " ";
+            cout << array[r][c]* scale;
         }
         cout << endl;
     }
