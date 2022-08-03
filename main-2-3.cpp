81837fb12b056e50c6087aa77acd8419e3465b5e
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-03 14:14:06 +0930

    code

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 extern int sum_if_palindrome(int [],int);
+extern bool is_palindrome(int [], int);
+extern int sum_array_elements(int [], int);
 int main(){
-    int array[3] = {5,10,5};
+    int array[3] = {5,10,2};
     int size = 3;
     cout << sum_if_palindrome(array,size) << endl;
+    cout << is_palindrome(array,size) << endl;
+    cout << sum_array_elements(array,size) << endl;
     return 0;
 }
