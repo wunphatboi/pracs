94ce521096dea225efca6d0df4b1fae2d34fdc87
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-03 14:13:54 +0930

    code

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int sum_if_palindrome(int integers[], int length){
     int sum = 0;
     for (int i = 0; i <= length/2; i++){
@@ -8,6 +9,24 @@ int sum_if_palindrome(int integers[], int length){
         }
     }
         for (int i = 0; i < length; i++){
+                sum = sum + integers[i];
+            }
+    return sum;
+}
+
+bool is_palindrome(int integers[], int length){
+    int sum = 0;
+    for (int i = 0; i <= length/2; i++){
+        if (integers[i] != integers[length-i-1] || length < 1 ){
+            return false;
+        }
+    }
+    return true;
+}
+
+int sum_array_elements(int integers[], int length){
+    int sum = 0;
+    for (int i = 0; i < length; i++){
                 sum = sum + integers[i];
             }
     return sum;
