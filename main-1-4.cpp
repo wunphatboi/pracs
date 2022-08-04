934ffa527588e8edd9bcdeeedafbba4f88847aaf
wunphatboi <104746077+wunphatboi@users.noreply.github.com>
2022-08-04 12:46:23 +0930

    Delete main-1-4.cpp

--- main-1-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-extern void print_scaled(int array[3][3],int);
-int main (){
-    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int scale = 3;
-    print_scaled(matrix,3);
-    return 0;
-}
