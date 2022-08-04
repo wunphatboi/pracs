e45e619e1980dc9472c9ff6e905a4c802d228425
wunphatboi <104746077+wunphatboi@users.noreply.github.com>
2022-08-04 12:46:17 +0930

    Delete main-1-3.cpp

--- main-1-3.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-extern void count_digits(int array[4][4]);
-int main (){
-    int matrix[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-    count_digits(matrix);
-    return 0;
-}
