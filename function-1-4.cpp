58b239ae5448274f7f9d7b119e1c20b36e0720c8
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-16 12:21:13 +0930

    code

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,8 +4,4 @@ void copy_doubles(double *old_array,double *new_array,int length){
     for (int i = 0;i < length; i++){
         *(new_array+i) = *(old_array+i);
     }
-    for (int i = 0;i < length; i++){
-        cout << *(new_array+i) << endl; 
-    }
-
 }
