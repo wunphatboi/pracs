9e6b047151de5ac6ac081ebe70f6a19285046d26
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-16 13:16:57 +0930

    code

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,7 +4,6 @@ extern int max_sub_sum(int *nums,int length);
 int main(){
     int arr1[10] = {31, -41, 59, 26, -53, 58, 97, -93, -23, 84};
     int arr2[10] = {-2,-2,-2,-2,-2,-2,-2,-2,-2,-2};
-    cout << max_sub_sum(arr1,10) << endl;
-    cout << max_sub_sum(arr2,10) << endl;
+    max_sub_sum(arr2,10);
     return 0;
 }
