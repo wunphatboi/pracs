4790886e243b5f3c8451d8ba9da936663c46abb2
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-16 10:34:56 +0930

    code

---
+++ function-1-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+void string_2d_copy(std::string first[][2], std::string second[][2], int n){
+    for (int r = 0; r < n; r++){
+        for (int c = 0; c < 2; c++){
+            second[r][c] = first[r][c];
+        }
+    }
+    for (int r = 0; r < n; r++){
+        for (int c = 0; c < 2; c++){
+            cout << second[r][c] << " ";
+        }
+        cout << endl;
+    }
+}
