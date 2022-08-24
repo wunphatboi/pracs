eb9bb47e6c3e60fafdee7992003a63c95f14108e
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-24 14:52:25 +0930

    code

--- main-4-1.cpp
+++ main-4-1.cpp
@@ -6,6 +6,7 @@ int main(){
     int row2[]={100,5,73};
     int row3[]={23,56,1};
     int *vals[]={row1,row2,row3};
-    int *mm = matrix_min_max(vals,3,3);
+    cout << *matrix_min_max(vals,3,3) << " " << *(matrix_min_max(vals,3,3)+1);
+    cout << endl;
     return 0;
 }
