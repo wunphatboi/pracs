f53e6e147ff1f17ce961d3a4236c821bb28d8f6e
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-30 00:05:25 +0930

    code

--- Wagon.h
+++ Wagon.h
@@ -1,17 +1,13 @@
-#ifndef WAGON_H
-#define WAGON_H
 #include <iostream>
 #include "Capybara.h"
-#include "Capybara.cpp"
 using namespace std;
 class Wagon{
     private:
     int counts;
-    public:
     Capybara w[5];
+    public:
     Wagon();                  
     bool addCapybara(Capybara newCapy);
     void emptyWagon();       
     void printCapybaras();
 };
-#endif
