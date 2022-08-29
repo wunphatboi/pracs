6791f9f439a448a80dd5371b88ab24973c56865d
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-29 09:49:40 +0930

    code

---
+++ Person.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Person{
+    private:
+    string name;
+    int salary;
+    public:
+    Person(int mySalary, string myName){
+        name = myName;
+        salary = mySalary;
+    }
+    void setName(string myName){
+        name = myName;
+    }
+    string getName(){
+        return name;
+    }
+    void setSalary(int mySalary){
+        salary = mySalary;
+    }
+    int getSalary(){
+        return salary;
+    }
+};
