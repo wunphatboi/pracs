485db40d3ad89fa16b37b4281a762697d4a83f4d
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-30 00:05:17 +0930

    code

--- Wagon.cpp
+++ Wagon.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
 #include "Wagon.h"
-#include "Capybara.h"
 using namespace std;
-class Wagon{
-    private:
-    int counts;
-    public:
-    Capybara w[5];
-    Wagon(){
+Wagon::Wagon(){
         counts = 0;
     }                            
-    bool addCapybara(Capybara newCapy){
-        w[counts] = newCapy;
+    bool Wagon::addCapybara(Capybara newCapy){
         if (counts == 4){
             cout << "cart is full" << endl;
+            w[counts++] = newCapy;
             return false;
         }else{
-            counts = counts +1;
+            w[counts++] = newCapy;
             return true;
         }
     }    
-    void emptyWagon(){
+    void Wagon::emptyWagon(){
         for(int i=0;i<counts;i++){
             w[i].~Capybara();
             counts=0;
             }
-    }                  
-    void printCapybaras(){
+    }               
+    void Wagon::printCapybaras(){
         for (int i = 0; i < counts; i++){
-            cout << getName.w[i] << " " << getAge.w[i] << endl;
+            cout << w[i].getName() << " " << w[i].getAge() << endl;
         }
     } 
-};
