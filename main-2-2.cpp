c659aa64afd52d41a99ff6077177743d15dd0b66
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-08-31 14:58:32 +0930

    code

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -5,9 +5,8 @@ int main(){
     Person p1(1000,"oliver");
     Person p2(100,"abby");
     Person p3(10,"mason");
-    Airplane A("maverick", p1, p2);
-    A.setPilot(p3); 
+    Airplane A("plane", p1, p2);
+    A.setPilot(p3);
     A.setCoPilot(p1);
-    A.printDetails();
     return 0;
 }
