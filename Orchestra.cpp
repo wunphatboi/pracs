80019867e17a4788b47f7e8e0228a90fe47d1c13
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-09-06 09:18:52 +0930

    code

---
+++ Orchestra.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "Orchestra.h"
+using namespace std;
+Orchestra::Orchestra(){
+    max_size = 0;
+    curr_size = 0;
+    members=new Musician[max_size];
+};
+Orchestra::Orchestra(int size){
+    size = max_size;
+    curr_size = 0;
+    members = new Musician[size];
+};
+int Orchestra::get_current_number_of_members(){
+    return curr_size;
+};
+bool Orchestra::has_instrument(std::string instrument){
+    for (int i = 0; i < curr_size; i++){
+        if (members[i].get_instrument() == instrument){
+            return true;
+        }
+    }
+    return false;
+};
+Musician* Orchestra::get_members(){
+    return members;
+};
+bool Orchestra::add_musician(Musician new_musician){
+    if (curr_size < max_size){
+        members[curr_size] = new_musician;
+        curr_size++;
+        return true;
+    }
+    return false;
+};
+Orchestra::~Orchestra(){
+    if(members != NULL){
+        delete []members;
+    }
+};
