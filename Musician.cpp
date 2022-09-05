ce461f49ae56c2a5758d2f8fdb8c94f91baa3f3a
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-09-05 22:55:35 +0930

    code

---
+++ Musician.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Musician.h"
+using namespace std;
+Musician::Musician(){
+    std::string instrument = NULL;
+    int experience = 0;
+};
+Musician::Musician(std::string instrument, int experience){
+    Instrument = instrument;
+    Experience = experience;
+};
+std::string Musician::get_instrument(){
+    return Instrument;
+};
+int Musician::get_experience(){
+    return Experience;
+};
