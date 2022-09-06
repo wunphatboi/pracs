086408932341c9a56553a60fe4513b7555470ffc
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-09-06 21:51:02 +0930

    code

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -2,11 +2,13 @@
 #include "Orchestra.h"
 using namespace std;
 int main(){
-    Orchestra O1(3);
-    Musician Ramin("violin",20);
-    Musician George("piano",10);
-    O1.add_musician(Ramin);
-    O1.add_musician(George);
-    O1.get_current_number_of_members();
+    Orchestra O1(4);
+    Musician Person1("violin",20);
+    Musician Person2("piano",10);
+    O1.add_musician(Person1);
+    O1.add_musician(Person2);
+    cout << O1.get_current_number_of_members() << endl;
+    cout << O1.has_instrument("guitar") << endl;
+    O1.~Orchestra();
     return 0;
 }
