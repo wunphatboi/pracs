ac2bb9bef4cbdd5707f6ef707eef9697409c2cf4
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-10-24 23:05:13 +1030

    code

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -2,5 +2,10 @@
 #include "hunter.h"
 using namespace std;
 int main(){
+    hunter h("hello",18);
+    std::cout << "name: " << h.get_name() << ", kills: " << h.get_kill() << std::endl ;
+    std::cout << "name: " << h.get_name() << ", kills: " << h.get_kill() << std::endl ;
+    h.set_kill(17);
+    std::cout << "name: " << h.get_name() << ", kills: " << h.get_kill() << std::endl ;
     return 0;
 }
