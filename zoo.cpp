7c97946256c55211027a8530c3a0f7bfaaaf951a
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-10-24 23:33:16 +1030

    code

--- zoo.cpp
+++ zoo.cpp
@@ -1,57 +1,42 @@
-// Include the required header files.
-
 #include "zoo.h"
+#include "vegie.h"
+#include "hunter.h"
+#include <iostream>
 
-// Define the constructor.
+zoo::zoo(){
+name="defaultzooname";
+number_of_animals=0;
+}
 
-zoo::zoo(string n,int cows,int lions)
-
-{
-
+zoo::zoo(std::string n,int cows,int lions){
 name=n;
-
 number_of_animals=cows+lions;
-
 animals = new animal*[number_of_animals];
 
 for(int i=0;i<number_of_animals;i++){
-
 if(i<cows){
-
 vegie* temp = new vegie("Daisy",100);
-
 animals[i]=temp;
-
 }
-
 else{
-
 hunter* temp = new hunter("Clarence",50);
-
 animals[i]=temp;
-
+}
+}
 }
 
-}
-
-}
-
-// Define the member functions.
-
-string zoo::get_name(){
-
+std::string zoo::get_name(){
 return name;
-
 }
 
 int zoo::get_number_of_animals(){
-
 return number_of_animals;
-
 }
 
 animal** zoo::get_animals(){
-
 return animals;
+}
 
+zoo::~zoo(){
+delete[] animals;
 }
