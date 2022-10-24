9e923bf5c647fa1d8da421e58dfed4a8b74ac031
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-10-24 22:41:03 +1030

    code

--- animal.h
+++ animal.h
@@ -1,18 +1,45 @@
+#ifndef ANIMAL_H
+
+#define ANIMAL_H
+
+// Include the required header files.
+
 #include<iostream>
+
 using namespace std;
 
+// Define the class.
+
 class animal{
-private :
-int volume;
+
 protected:
+
 string name;
+
 int animalID;
 
+int volume;
+
 public:
+
+// Declare the constructor.
+
+animal(string n ,int v);
+
+//Define the pure virtual function.
+
+virtual string get_name() = 0;
+
+// Declare the member functions.
+
 void set_name(string n);
-void set_volume(int vol);
-virtual string get_name()=0;
-int get_animalID();
+
+void set_volume(int v);
+
 int get_volume();
 
+int get_animalID();
+
 };
+
+#endif
