4f272f8a5580774cd9ce5be683fd00ff36c3f602
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2022-10-24 22:40:53 +1030

    code

--- animal.cpp
+++ animal.cpp
@@ -1,13 +1,41 @@
+// Include the required header files.
+
 #include "animal.h"
-void animal::set_name(string n){
-name = n;
+
+// Define the constructor.
+
+animal::animal(string n ,int v){
+
+animalID++;
+
+name=n;
+
+volume =v;
+
 }
-void animal::set_volume(int vol){
-volume = vol;
+
+// Define the member functions.
+
+void animal:: set_name(string n){
+
+name=n;
+
 }
-int animal::get_volume(){
+
+void animal:: set_volume(int v){
+
+volume =v;
+
+}
+
+int animal:: get_volume(){
+
 return volume;
+
 }
-int animal::get_animalID(){
+
+int animal:: get_animalID(){
+
 return animalID;
+
 }
